afd2a5c128384d73a6c7dd3ce0583abd4a032248
Zerio113 <zeriozerio60@gmail.com>
2023-12-19 15:06:35 +0100

    v2

--- seo_audit.py
+++ seo_audit.py
@@ -3,303 +3,140 @@ from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 import csv
 from datetime import datetime
+import tkinter as tk
+from tkinter import filedialog, messagebox
 
-# Fonction pour obtenir les occurrences des mots dans un texte
-def get_word_occurrences(text):
-    words = text.lower().split()
-    word_occurrences = {}
-    for word in words:
-        word_occurrences[word] = word_occurrences.get(word, 0) + 1
-    sorted_occurrences = sorted(word_occurrences.items(), key=lambda item: item[1], reverse=True)
-    return sorted_occurrences
+class SEOAnalyzer:
+    def __init__(self, url, keywords):
+        self.url = url
+        self.keywords = keywords
+        self.text = self.get_html_text()
+        self.soup = BeautifulSoup(self.text, 'html.parser')
 
-# Fonction pour supprimer les mots parasites
-def remove_parasite_words(word_occurrences, parasite_words):
-    cleaned_word_occurrences = [item for item in word_occurrences if item[0] not in parasite_words]
-    return cleaned_word_occurrences
+    def get_html_text(self):
+        response = requests.get(self.url)
+        return response.text
 
-# Fonction pour récupérer les mots parasites à partir du fichier CSV
-def get_parasite_words(file_path='parasite.csv'):
-    with open(file_path, 'r') as file:
-        parasite_words = file.read().split(',')
-    return [word.strip() for word in parasite_words]
+    def get_word_occurrences(self, text):
+        words = text.lower().split()
+        word_occurrences = {}
+        for word in words:
+            word_occurrences[word] = word_occurrences.get(word, 0) + 1
+        sorted_occurrences = sorted(word_occurrences.items(), key=lambda item: item[1], reverse=True)
+        return sorted_occurrences
 
-# Fonction pour effectuer l'audit SEO complet
-def perform_seo_audit(text, url):
-    # Utiliser la fonction pour enlever les balises HTML
-    text_without_html = remove_html_tags(text)
+    def remove_parasite_words(self, word_occurrences, parasite_words):
+        cleaned_word_occurrences = [item for item in word_occurrences if item[0] not in parasite_words]
+        return cleaned_word_occurrences
 
-    # Récupération des mots clés importants
-    occurrences = get_word_occurrences(text_without_html)
-    parasite_list = get_parasite_words()
-    cleaned_occurrences = remove_parasite_words(occurrences, parasite_list)
+    def get_parasite_words(self, file_path='parasite.csv'):
+        with open(file_path, 'r') as file:
+            parasite_words = file.read().split(',')
+        return [word.strip() for word in parasite_words]
 
-    # Extraction des liens entrants et sortants ainsi que des balises alt
-    soup = BeautifulSoup(text, 'html.parser')
-    all_links = get_tag_attribute_values(text, 'a', 'href')
-    num_incoming_links, num_outgoing_links = len(all_links), len(all_links)
-    alt_values = get_alt_tags_details(soup)
-    SHOW_MORE_BUTTON_TEXT = "Afficher Plus"
-    DOWNLOAD_CSV_BUTTON_TEXT = "Télécharger CSV"
-    # Génération du rapport d'audit au format HTML
-    report = f'''
-    <html>
-    <head>
-        <title>Rapport d'Audit SEO</title>
-        <style>
-            body {{
-                font-family: Arial, sans-serif;
-                line-height: 1.6;
-                padding: 20px;
-                background-color: #f2f2f2;
-            }}
-            h1, h2, p, ul, li, table {{
-                margin: 0;
-                padding: 0;
-            }}
-            h1 {{
-                font-size: 36px;
-                text-align: center;
-                margin-bottom: 20px;
-                color: #333;
-            }}
-            h2 {{
-                font-size: 24px;
-                margin-top: 40px;
-                margin-bottom: 10px;
-                color: #333;
-            }}
-            p, li {{
-                font-size: 16px;
-                color: #555;
-            }}
-            ul {{
-                margin-top: 10px;
-                list-style-type: none;
-            }}
-            table {{
-                width: 80%;
-                border-collapse: collapse;
-                margin: 20px auto;
-                background-color: #fff;
-            }}
-            th, td {{
-                border: 1px solid #ddd;
-                padding: 8px;
-                text-align: left;
-            }}
-            th {{
-                background-color: #f4f4f4;
-            }}
-            img {{
-                display: block;
-                margin: 20px auto;
-                max-width: 150px;
-                height: auto;
-            }}
-            .content-table {{
-                display: table;
-            }}
-            .visible-table {{
-                display: table;
-            }}
-            .hidden-row {{
-                display: none;
-            }}
-            .toggle-button {{
-                background-color: #4CAF50;
-                border: none;
-                color: white;
-                padding: 10px 20px;
-                text-align: center;
-                text-decoration: none;
-                display: block;
-                margin: 20px 10px;
-                cursor: pointer;
-            }}
-        </style>
-        <script>
-            var showMoreButtonText = "{SHOW_MORE_BUTTON_TEXT}";
-            var downloadCSVButtonText = "{DOWNLOAD_CSV_BUTTON_TEXT}";
-            function toggleTable(tableId) {{
-                var table = document.getElementById(tableId);
-                var toggleButton = table.previousElementSibling;
-                var rows = table.getElementsByTagName('tr');
-                for (var i = 10; i < rows.length; i++) {{
-                    if (rows[i].classList.contains('hidden-row')) {{
-                        rows[i].classList.remove('hidden-row');
-                    }} else {{
-                        rows[i].classList.add('hidden-row');
-                    }}
-                }}
-                if (table.classList.contains('content-table')) {{
-                    table.classList.remove('content-table');
-                    table.classList.add('visible-table');
-                    toggleButton.innerText = 'Afficher Moins';
-                }} else {{
-                    table.classList.remove('visible-table');
-                    table.classList.add('content-table');
-                    toggleButton.innerText = 'Afficher Plus';
-                }}
-            }}
-            function sortTable(tableId, col, asc) {{
-                var table, rows, switching, i, x, y, shouldSwitch;
-                table = document.getElementById(tableId);
-                switching = true;
-                while (switching) {{
-                    switching = false;
-                    rows = table.rows;
-                    for (i = 1; i < (rows.length - 1); i++) {{
-                        shouldSwitch = false;
-                        x = rows[i].getElementsByTagName("td")[col];
-                        y = rows[i + 1].getElementsByTagName("td")[col];
-                        if (asc) {{
-                            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {{
-                                shouldSwitch = true;
-                                break;
-                            }}
-                        }} else {{
-                            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {{
-                                shouldSwitch = true;
-                                break;
-                            }}
-                        }}
-                    }}
-                    if (shouldSwitch) {{
-                        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-                        switching = true;
-                    }}
-                }}
-            }}
-            function downloadTableAsCSV(tableId, filename) {{
-                var csv = [];
-                var rows = document.querySelectorAll("#" + tableId + " tr");
-                for (var i = 0; i < rows.length; i++) {{
-                    var row = [], cols = rows[i].querySelectorAll("td, th");
-                    for (var j = 0; j < cols.length; j++)
-                        row.push(cols[j].innerText);
-                    csv.push(row.join(","));
-                }}
-                // Download CSV file
-                downloadCSV(csv.join("\\n"), filename);
-            }}
+    def remove_html_tags(self, html_text):
+        soup = BeautifulSoup(html_text, 'html.parser')
+        return soup.get_text()
 
-            function downloadCSV(csv, filename) {{
-                var csvFile;
-                var downloadLink;
+    def get_tag_attribute_values(self, tag_name, attribute_name):
+        values = [tag.get(attribute_name) for tag in self.soup.find_all(tag_name)]
+        return values
 
-                // CSV file
-                csvFile = new Blob([csv], {{type: "text/csv"}});
+    def get_alt_tags_details(self):
+        alt_values = []
+        all_img_tags = self.soup.find_all('img')
+        for img in all_img_tags:
+            alt = img.get('alt')
+            link_url = img.parent.get('href') if img.parent.name == 'a' else 'N/A'
+            alt_values.append({
+                'alt_text': alt,
+                'occurrences': sum(1 for t in alt_values if t == alt),
+                'link_url': link_url
+            })
+        return alt_values
 
-                // Download link
-                downloadLink = document.createElement("a");
+    def perform_seo_audit(self):
+        text_without_html = self.remove_html_tags(self.text)
+        occurrences = self.get_word_occurrences(text_without_html)
+        parasite_list = self.get_parasite_words()
+        cleaned_occurrences = self.remove_parasite_words(occurrences, parasite_list)
+        all_links = self.get_tag_attribute_values('a', 'href')
+        num_incoming_links, num_outgoing_links = len(all_links), len(all_links)
+        alt_values = self.get_alt_tags_details()
+        return cleaned_occurrences, all_links, num_incoming_links, num_outgoing_links, alt_values
 
-                // File name
-                downloadLink.download = filename;
+class Application(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.title("SEO Analyzer")
+        self.geometry("500x300")
 
-                // Create a link to the file
-                downloadLink.href = window.URL.createObjectURL(csvFile);
+        # Add a title label
+        title_label = tk.Label(self, text="SEO Analyzer", font=("Helvetica", 16, "bold"))
+        title_label.pack(pady=10)
 
-                // Hide download link
-                downloadLink.style.display = "none";
+        # URL Entry
+        self.url_label = tk.Label(self, text="Enter URL:")
+        self.url_label.pack()
+        self.url_entry = tk.Entry(self, width=40)
+        self.url_entry.pack(pady=5)
 
-                // Add the link to DOM
-                document.body.appendChild(downloadLink);
+        # Keywords Entry
+        self.keywords_label = tk.Label(self, text="Enter Keywords (comma-separated):")
+        self.keywords_label.pack()
+        self.keywords_entry = tk.Entry(self, width=40)
+        self.keywords_entry.pack(pady=5)
 
-                // Click download link
-                downloadLink.click();
-            }}
-        </script>
-    </head>
-    <body>
-        <h1>Rapport d'Audit SEO</h1>
-        <img src="https://www.esiee-it.fr/themes/custom/generic/medias/logo-esiee-it.png" alt="ESIEE-IT Logo">
-        <p>Date d'audit : {datetime.now().strftime("%d-%m-%Y %H:%M:%S")}</p>
-        <h2>URL analysée :</h2>
-        <p>{url}</p>
-        <h2>Mots clés importants :</h2>
-        <button class="toggle-button" onclick="toggleTable('keywords-table')">Afficher Plus</button>
-        <button class="toggle-button" onclick="downloadTableAsCSV('keywords-table', 'keywords.csv')">Télécharger CSV</button>
-        <table id="keywords-table" class="content-table">
-            <tr>
-                <th>Mot clé</th>
-                <th>Occurrences</th>
-            </tr>
-            {''.join(f"<tr><td>{keyword}</td><td>{occurrence}</td></tr>" for keyword, occurrence in cleaned_occurrences[:10])}
-            {''.join(f"<tr class='hidden-row'><td>{keyword}</td><td>{occurrence}</td></tr>" for keyword, occurrence in cleaned_occurrences[10:])}
-        </table>
-        <h2>Liens entrants :</h2>
-        <button class="toggle-button" onclick="toggleTable('incoming-links-table')">Afficher Plus</button>
-        <button class="toggle-button" onclick="downloadTableAsCSV('incoming-links-table', 'incoming_links.csv')">Télécharger CSV</button>
-        <table id="incoming-links-table" class="content-table">
-            <tr>
-                <th>URL</th>
-                <th>Occurrences</th>
-            </tr>
-            {''.join(f"<tr><td>{link}</td><td>{all_links.count(link)}</td></tr>" for link in all_links[:10])}
-            {''.join(f"<tr class='hidden-row'><td>{link}</td><td>{all_links.count(link)}</td></tr>" for link in all_links[10:])}
-        </table>
-        <h2>Liens sortants :</h2>
-        <button class="toggle-button" onclick="toggleTable('outgoing-links-table')">Afficher Plus</button>
-        <button class="toggle-button" onclick="downloadTableAsCSV('outgoing-links-table', 'outgoing_links.csv')">Télécharger CSV</button>
-        <table id="outgoing-links-table" class="content-table">
-            <tr>
-                <th>URL</th>
-                <th>Occurrences</th>
-            </tr>
-            {''.join(f"<tr><td>{link}</td><td>{all_links.count(link)}</td></tr>" for link in all_links[:10])}
-            {''.join(f"<tr class='hidden-row'><td>{link}</td><td>{all_links.count(link)}</td></tr>" for link in all_links[10:])}
-        </table>
-        <h2>Balises alt :</h2>
-        <button class="toggle-button" onclick="toggleTable('alt-table')">Afficher Plus</button>
-        <button class="toggle-button" onclick="downloadTableAsCSV('alt-table', 'alt_tags.csv')">Télécharger CSV</button>
-        <table id="alt-table" class="content-table">
-            <tr>
-                <th>Balise Alt</th>
-                <th>Nombre d'Occurrences</th>
-                <th>URL Liée</th>
-            </tr>
-            {''.join(f"<tr><td>{alt['alt_text']}</td><td>{alt['occurrences']}</td><td>{alt['link_url']}</td></tr>" for alt in alt_values)}
-        </table>
-    </body>
-    </html>
-    '''
+        # Analyze Button
+        self.analyze_button = tk.Button(self, text="Analyze", command=self.analyze, bg="green", fg="white")
+        self.analyze_button.pack(pady=10)
 
-    with open('audit_report.html', 'w', encoding='utf-8') as file:
-        file.write(report)
+    def analyze(self):
+        url = self.url_entry.get()
+        keywords = self.keywords_entry.get().split(',')
+        self.seo_analyzer = SEOAnalyzer(url, keywords)
+        self.destroy()
+        self.results_window = ResultsWindow(self.seo_analyzer)
 
-# Fonction pour supprimer les balises HTML d'un texte
-def remove_html_tags(html_text):
-    soup = BeautifulSoup(html_text, 'html.parser')
-    return soup.get_text()
+class ResultsWindow(tk.Tk):
+    def __init__(self, seo_analyzer):
+        tk.Tk.__init__(self)
+        self.title("SEO Results")
+        self.geometry("800x600")
+        self.seo_analyzer = seo_analyzer
+        self.create_widgets()
 
-# Fonction pour obtenir les valeurs des attributs d'une balise
-def get_tag_attribute_values(html_text, tag_name, attribute_name):
-    soup = BeautifulSoup(html_text, 'html.parser')
-    values = [tag.get(attribute_name) for tag in soup.find_all(tag_name)]
-    return values
+    def create_widgets(self):
+        cleaned_occurrences, all_links, num_incoming_links, num_outgoing_links, alt_values = self.seo_analyzer.perform_seo_audit()
 
-# Fonction pour extraire les détails des balises alt
-def get_alt_tags_details(soup):
-    alt_values = []
-    all_img_tags = soup.find_all('img')
-    for img in all_img_tags:
-        alt = img.get('alt')
-        link_url = img.parent.get('href') if img.parent.name == 'a' else 'N/A'
-        alt_values.append({
-            'alt_text': alt,
-            'occurrences': sum(1 for t in alt_values if t == alt),
-            'link_url': link_url
-        })
-    return alt_values
+        # Results Text
+        self.results_text = tk.Text(self, wrap=tk.WORD)
+        self.results_text.pack(expand=True, fill=tk.BOTH, padx=10, pady=10)
 
-# Programme principal pour l'audit de la première page
-def main():
-    url = input("Veuillez saisir l'URL de la page à analyser : ")
-    response = requests.get(url)
-    html_text = response.text
-    perform_seo_audit(html_text, url)
-    print("Le rapport d'audit a été généré avec succès.")
+        # Insert Results
+        self.results_text.insert(tk.END, f"Number of incoming links: {num_incoming_links}\n", "big")
+        self.results_text.insert(tk.END, f"Number of outgoing links: {num_outgoing_links}\n", "big")
+
+        self.results_text.insert(tk.END, "\nAlt tags details:\n", "big")
+        for alt in alt_values:
+            self.results_text.insert(tk.END, f"{alt}\n")
+
+        self.results_text.insert(tk.END, "\nKeywords occurrences:\n", "big")
+        for word, count in cleaned_occurrences:
+            self.results_text.insert(tk.END, f"{word}: {count}\n")
+
+        # Save Button
+        self.save_button = tk.Button(self, text="Save Report", command=self.save_report, bg="blue", fg="white")
+        self.save_button.pack(pady=10)
+
+        # Tag Configurations
+        self.results_text.tag_configure("big", font=("Helvetica", 12, "bold"))
+
+    def save_report(self):
+        file_path = filedialog.asksaveasfilename(defaultextension=".txt")
+        with open(file_path, 'w') as file:
+            file.write(self.results_text.get(1.0, tk.END))
 
 if __name__ == "__main__":
-    main()
+    app = Application()
+    app.mainloop()
